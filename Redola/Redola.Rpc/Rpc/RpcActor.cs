5ad69f2ff2e7590f0e60663ee2c1a64ed576bba3
gaochundong <gaochundong@hotmail.com>
2016-10-17 14:06:22 +0800

    implements the rpc service

---
+++ Redola/Redola.Rpc/Rpc/RpcActor.cs
@@ -0,0 +1,15 @@
+﻿using Redola.ActorModel;
+
+namespace Redola.Rpc
+{
+    public class RpcActor : BlockingRouteActor
+    {
+        public RpcActor(
+            ActorConfiguration configuration,
+            IActorMessageEncoder encoder,
+            IActorMessageDecoder decoder)
+            : base(configuration, encoder, decoder)
+        {
+        }
+    }
+}
---
+++ Redola/Redola.Rpc/Rpc/RpcService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Redola.Rpc
+{
+    public abstract class RpcService : BlockingActorMessageHandlerBase
+    {
+        public RpcService(RpcActor localActor)
+            : base(localActor)
+        {
+        }
+
+        protected sealed override void RegisterAdmissibleMessages(IDictionary<string, MessageHandleStrategy> admissibleMessages)
+        {
+            base.RegisterAdmissibleMessages(admissibleMessages);
+
+            var rpcMessages = RegisterRpcMessages();
+            if (rpcMessages != null)
+            {
+                foreach (var pair in rpcMessages)
+                {
+                    admissibleMessages.Add(pair.Item2.Name, new MessageHandleStrategy(pair.Item2));
+                }
+            }
+        }
+
+        protected abstract IEnumerable<Tuple<Type, Type>> RegisterRpcMessages();
+    }
+}
