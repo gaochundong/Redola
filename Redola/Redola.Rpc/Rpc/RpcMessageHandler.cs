1911de69aaa0af86c546cd4b3afbf62dbf3b4ecc
gaochundong <gaochundong@hotmail.com>
2016-10-17 13:43:17 +0800

    upload the protobuf tools

---
+++ Redola/Redola.Rpc/Rpc/RpcMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Logrila.Logging;
+
+namespace Redola.Rpc
+{
+    public class RpcMessageHandler : BlockingActorMessageHandlerBase
+    {
+        private ILog _log = Logger.Get<RpcMessageHandler>();
+
+        public RpcMessageHandler(BlockingRouteActor localActor)
+            : base(localActor)
+        {
+        }
+
+        protected override void RegisterAdmissibleMessages(IDictionary<string, MessageHandleStrategy> admissibleMessages)
+        {
+            base.RegisterAdmissibleMessages(admissibleMessages);
+        }
+    }
+}
