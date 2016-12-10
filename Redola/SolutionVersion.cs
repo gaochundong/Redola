a7f072276b4227c3158efe55925d8f5cafafa390
gaochundong <gaochundong@hotmail.com>
2016-12-10 20:37:56 +0800

    change the version first before refactoring

--- Redola/SolutionVersion.cs
+++ Redola/SolutionVersion.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
 [assembly: ComVisible(false)]
