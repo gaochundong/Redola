52fe946e624236bf3592c1927dc2796a7f99ed5e
gaochundong <gaochundong@hotmail.com>
2017-07-27 11:25:14 +0800

    Duplicate service entry for service type

---
+++ Redola/Redola.Rpc/Rpc/Module/Catalog/ServiceCatalogProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Redola.Rpc
+{
+    public class ServiceCatalogProvider : IServiceCatalogProvider
+    {
+        private List<ServiceEntry> _entries = new List<ServiceEntry>();
+
+        public ServiceCatalogProvider()
+        {
+        }
+
+        public void RegisterService<T>(T service)
+        {
+            RegisterService(typeof(T), service);
+        }
+
+        public void RegisterService(Type declaringType, object service)
+        {
+            if (declaringType == null)
+                throw new ArgumentNullException("declaringType");
+            if (service == null)
+                throw new ArgumentNullException("service");
+
+            lock (_entries)
+            {
+                if (_entries.Any(e => e.DeclaringType.Equals(declaringType)))
+                    throw new ArgumentException(string.Format(
+                        "Duplicate service entry for service type [{0}].", declaringType), "declaringType");
+                _entries.Add(new ServiceEntry(declaringType, service));
+            }
+        }
+
+        public IEnumerable<ServiceEntry> GetServices()
+        {
+            return _entries;
+        }
+    }
+}
